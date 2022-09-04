663a8aeb5358d3eb629f0218c830fff7523e2837
Vinicius_Red <atalibavinicius@gmail.com>
2022-09-04 00:21:38 -0300

    Update

--- build.bat
+++ build.bat
@@ -1,16 +1,16 @@
 @echo off
 for /f "delims==; tokens=1,2 eol=;" %%G in (module.prop) do set %%G=%%H
+title Creating Module Zip %version%
 set folders=test
-echo %folders%
 pause
 cls
 goto :eof
 
-title Creating Module Zip %version%
+:build
 if exist "%SYSTEMDRIVE%\Program Files" (
-  echo %version%
+  echo Creating Module Wifi_Bonding  %version%
   "C:\Program Files\7-Zip\7z.exe" %id% %version%
 ) else (
-  echo %version%
+  echo Creating Module Wifi_Bonding %version%
   "C:\Program Files (x86)\7-Zip\7z.exe" %id% %version%
 )
