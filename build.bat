2ea63131774b8da99c40e2440291e581c6105fc0
Vinicius_Red <atalibavinicius@gmail.com>
2022-09-05 18:38:26 -0300

    Update

--- build.bat
+++ build.bat
@@ -1,16 +1,17 @@
 @echo off
 for /f "delims==; tokens=1,2 eol=;" %%G in (module.prop) do set %%G=%%H
 title Creating Module Zip %version%
-set folders=test
+set files=META-INF customize.sh module.prop
+call :build
 pause
 cls
 goto :eof
 
 :build
-if exist "%SYSTEMDRIVE%\Program Files" (
-  echo Creating Module Wifi_Bonding  %version%
-  "C:\Program Files\7-Zip\7z.exe" %id% %version%
+if exist "%SYSTEMDRIVE%\Program Files (x86)" (
+  echo Creating Module Wifi_Bonding %version%
+  "C:\Program Files\7-Zip\7z.exe" a -mmt2 -mx9 %id%-%version%.zip %files%
 ) else (
   echo Creating Module Wifi_Bonding %version%
-  "C:\Program Files (x86)\7-Zip\7z.exe" %id% %version%
+  "C:\Program Files (x86)\7-Zip\7z.exe" a -mmt2 -mx9 %id%-%version%.zip %files%
 )
