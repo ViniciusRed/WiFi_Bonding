bcbf40bcad85c131f842679f911a01a5bd3f4c3b
Vinicius_Red <atalibavinicius@gmail.com>
2022-09-05 20:49:34 -0300

    Update

--- build.bat
+++ build.bat
@@ -3,7 +3,6 @@ for /f "delims==; tokens=1,2 eol=;" %%G in (module.prop) do set %%G=%%H
 title Creating Module Zip %version%
 set files=META-INF customize.sh module.prop
 call :build
-pause
 cls
 goto :eof
 
