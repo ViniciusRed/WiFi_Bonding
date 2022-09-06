569e34ed555ef409c7009e64159878e0d8e6e2f9
Vinicius_Red <atalibavinicius@gmail.com>
2022-09-06 10:13:45 -0300

    Update

--- build.bat
+++ build.bat
@@ -2,5 +2,7 @@
 for /f "delims==; tokens=1,2 eol=;" %%G in (module.prop) do set %%G=%%H
 title Creating Module Zip %version%
 set files=META-INF customize.sh module.prop
+jq '.list |= map(select(.version != "%version%"))' .github/update.json
+jq '.list |= map(select(.versionCode != "%versionCode%"))' .github/update.json
 echo Creating Module Wifi_Bonding %version%
 7z a -mmt2 -mx9 %id%-%version%.zip %files%
