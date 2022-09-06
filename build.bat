49b7267259d5fa079a6bf0b0bbeb6d0644f43bce
Vinicius_Red <atalibavinicius@gmail.com>
2022-09-06 10:02:01 -0300

    Update

--- build.bat
+++ build.bat
@@ -1,8 +1,5 @@
 @echo off
 for /f "delims==; tokens=1,2 eol=;" %%G in (module.prop) do set %%G=%%H
-for /f "delims==; tokens=1,2 eol=;" %%G in (.github/update.json) do set %%G=%%H
-echo %zipUrl%
-pause
 title Creating Module Zip %version%
 set files=META-INF customize.sh module.prop
 echo Creating Module Wifi_Bonding %version%
