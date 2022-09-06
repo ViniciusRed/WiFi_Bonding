ceb958e595ad55ee1861548db5ba99db3a2cd6fc
Vinicius_Red <atalibavinicius@gmail.com>
2022-09-06 09:52:52 -0300

    Update

--- build.bat
+++ build.bat
@@ -4,5 +4,6 @@ title Creating Module Zip %version%
 set files=META-INF customize.sh module.prop
 jq '.list |= map(select(.version != "%version%"))' .github/update.json
 jq '.list |= map(select(.versionCode != "%versionCode%"))' .github/update.json
+jq '.list |= map(select(.zipUrl != "https://github.com/ViniciusRed/WiFi_Bonding/releases/latest/download/WiFi_Bonding-%version%.zip"))' .github/update.json
 echo Creating Module Wifi_Bonding %version%
 7z a -mmt2 -mx9 %id%-%version%.zip %files%
